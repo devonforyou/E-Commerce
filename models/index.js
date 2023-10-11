5658a5b4e43ab90f090ad22015a4e1e9631bfc61
Devon Hua <huadevon@gmail.com>
2023-10-11 09:38:01 -0500

    fixed syntax error in index.js

--- models/index.js
+++ models/index.js
@@ -21,7 +21,7 @@ Product.belongsToMany(Tag, {
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: 'tag-id',
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
